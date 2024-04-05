9f24a4752ae1be76d5b58e9455dbc856222b0541
shallowrayn <shallowrain@proton.me>
2024-04-05 22:46:25 +0100

    Add support for windows

--- build.rs
+++ build.rs
@@ -1,18 +1,52 @@
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(target_os = "windows")]
+fn get_windows_path() -> PathBuf {
+    #[cfg(target_pointer_width = "64")]
+        let program_files = PathBuf::from(r"C:\Program Files");
+    #[cfg(target_pointer_width = "32")]
+        let program_files = PathBuf::from(r"C:\Program Files (x86)");
+    for entry in std::fs::read_dir(program_files).expect("Failed to read Program Files directory") {
+        let entry = entry.expect("Failed to read entry");
+        let path = entry.path();
+        if path.is_dir() {
+            if let Some(dir_name) = path.file_name() {
+                if dir_name.to_string_lossy().starts_with("QBDI") {
+                    return path;
+                }
+            }
+        }
+    }
+    panic!("Failed to find QBDI installation directory")
+}
+
 fn get_lib_name() -> String {
     if let Ok(lib_name) = env::var("QBDI_LIB_NAME") {
         return lib_name;
     }
 
-    // "QBDI_static"
-    "QBDI".to_owned()
+    // Avoid needing to copy libQBDI.dll to the target directory
+    // TODO: Is this a sensible default?
+    if cfg!(target_os = "windows") {
+        "QBDI_static".to_owned()
+    } else {
+        "QBDI".to_owned()
+    }
 }
 
 fn get_lib_dir() -> Option<PathBuf> {
     if let Ok(lib_dir) = env::var("QBDI_LIB_DIR") {
         return Some(PathBuf::from(lib_dir));
+    }
+
+    #[cfg(target_os = "windows")]
+    {
+        let installation_path = get_windows_path();
+        let lib_dir = installation_path.join("lib");
+        if lib_dir.is_dir() {
+            return Some(lib_dir);
+        }
     }
 
     None
@@ -21,6 +55,15 @@ fn get_lib_dir() -> Option<PathBuf> {
 fn get_additional_include_dirs() -> Vec<PathBuf> {
     if let Ok(include_dir) = env::var("QBDI_INCLUDE_DIR") {
         return vec![PathBuf::from(include_dir)];
+    }
+
+    #[cfg(target_os = "windows")]
+    {
+        let installation_path = get_windows_path();
+        let include_dir = installation_path.join("include");
+        if include_dir.is_dir() {
+            return vec![include_dir];
+        }
     }
 
     vec![]
